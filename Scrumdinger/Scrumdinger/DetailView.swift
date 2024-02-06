f9b80e7a9147c993dd1050d0d006a54de543854e
DavidBermudez7 <DavidBermudez7@users.noreply.github.com>
2024-02-06 15:46:36 +0000

    Created DetailEditView. Updated DetailView

--- Scrumdinger/Scrumdinger/DetailView.swift
+++ Scrumdinger/Scrumdinger/DetailView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
+    
+    @State private var isPresentingEditView = false
+    
     var body: some View {
         List {
             Section {
@@ -49,6 +52,24 @@ struct DetailView: View {
 
         }
         .navigationTitle(scrum.title)
+        .toolbar {
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView, content: {
+            NavigationStack {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+            }
+        })
     }
 }
 
