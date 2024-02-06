5105640770f5d51c206a8a0b4208e076e535a738
DavidBermudez7 <DavidBermudez7@users.noreply.github.com>
2024-02-06 10:23:56 +0000

    Added the accessibility labels to the CardView

---
+++ Scrumdinger/Scrumdinger/LabelStyle.swift
@@ -0,0 +1,22 @@
+//
+//  LabelStyle.swift
+//  Scrumdinger
+//
+//  Created by David Bermudez Hidalgo on 25/01/2024.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
+
