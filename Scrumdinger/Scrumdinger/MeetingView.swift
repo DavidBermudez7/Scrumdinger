c4eaf00ff948f0570518e9d4e5f8a59c06aac2cc
DavidBermudez7 <DavidBermudez7@users.noreply.github.com>
2024-02-15 11:37:28 +0000

    Several changes
    Some parts of MeetingView have been isolated and created new file such as MeetingFooter and Meeting header.
    
    Both of this views are already completed

--- Scrumdinger/Scrumdinger/MeetingView.swift
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -6,53 +6,44 @@
 //
 
 import SwiftUI
-import CoreData
+import AVFoundation
 
 struct MeetingView: View {
 
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    
+    private var Player: AVPlayer { AVPlayer.sharedDingPlayer }
 
     var body: some View {
-        VStack {
-            ProgressView(value:10, total: 15)
-            HStack{
-                VStack(alignment: .leading) {
-                    Text("Seconds Ellapsed")
-                        .font(.caption)
-                    Label("300", systemImage: "hourglass.tophalf.fill")
-                }
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
                 
-                Spacer()
+                Circle()
+                    .strokeBorder(lineWidth: 24)
                 
-                VStack(alignment: .trailing) {
-                    Text("Seconds remaining")
-                        .font(.caption)
-                    Label("600", systemImage: "hourglass.bottomhalf.fill")
-                }
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
+                
             }
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel("Time remaining")
-            .accessibilityLabel("10 minutes")
-            
-            Circle()
-                .strokeBorder(lineWidth: 24)
-            
-            HStack {
-                Text("Speaker 1 of 3")
-                
-                Spacer()
-                
-                Button(action: {}) {
-                    Image(systemName: "forward.fill")
-                }
-                .accessibilityLabel("Next speaker")
-            }
-            
         }
         .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 
 #Preview {
-    MeetingView()
+    MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
 }
