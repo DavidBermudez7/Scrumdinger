c1bd32b3bde38c65a74ad461328bc73678682f18
DavidBermudez7 <DavidBermudez7@users.noreply.github.com>
2024-04-09 17:55:39 +0100

    Project Created. The MeetingView has been prototyped

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by David Bermudez Hidalgo on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
