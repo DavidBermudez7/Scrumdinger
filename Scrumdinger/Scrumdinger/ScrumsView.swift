21cc853f6715d297d68926b88e1dafdfe27f0426
DavidBermudez7 <DavidBermudez7@users.noreply.github.com>
2024-04-09 19:02:51 +0100

    Updated DailyScrum and created ScrumsView
    DailyScrum now complies with the identifiable protocol because an id property with an UUID.
    
    Scrums view is the UI for the scrums using Cardview on a list

---
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -0,0 +1,22 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by David Bermudez Hidalgo on 09/04/2024.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
